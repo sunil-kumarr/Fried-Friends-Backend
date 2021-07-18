8fbc4dd852c602fe14d65010b39e629142935c56
sunil-kumarr <sunindus.sk@gmail.com>
2021-07-18 15:43:39 +0530

    UPDATE | server.js to use routes

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to Friend Manager application." });
 });
 
+require("./app/routes/friend.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
